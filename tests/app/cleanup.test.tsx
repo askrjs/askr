590a695c8aa1da18b724e611b1e48ab9e643b81c
smiggleworth <jrepanich@gmail.com>
2025-12-21 15:32:43 -0500

    test: add cleanupApp test and refine cleanup API; dev: log cleanup errors; ssr: re-entrant guard

--- tests/app/cleanup.test.tsx
+++ tests/app/cleanup.test.tsx
@@ -1,13 +1,12 @@
 import { describe, it, expect } from 'vitest';
 import { createIsland, cleanupApp } from '../../src/index';
 import { createTestContainer } from '../helpers/test_renderer';
+import { registerMountOperation } from '../../src/runtime/component';
 
 describe('createApp cleanup', () => {
-  it('cleanupApp runs component cleanup functions', () => {
+  it('should run component cleanup functions when cleanupApp is called', () => {
     const { container, cleanup } = createTestContainer();
     let cleaned = false;
-
-    import { registerMountOperation } from '../../src/runtime/component';
 
     const Component = () => {
       // Register mount operation that returns a cleanup function
