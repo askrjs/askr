b1d775ef9f7409bb1ff50cc6f369e5275efa9f58
smiggleworth <jrepanich@gmail.com>
2026-03-24 15:45:51 -0400

    feat: add DebouncedInput component with debounce functionality and update input types

--- packages/askr-ui/src/components/input/index.ts
+++ packages/askr-ui/src/components/input/index.ts
@@ -1,6 +1,8 @@
-export { Input } from './input';
+export { DebouncedInput, Input } from './input';
 export { INPUT_A11Y_CONTRACT, type InputA11yContract } from './input.a11y';
 export type {
+  DebouncedInputProps,
+  InputEvent,
   InputProps,
   InputOwnProps,
   InputInputProps,
--- packages/askr-ui/src/components/input/input.types.ts
+++ packages/askr-ui/src/components/input/input.types.ts
@@ -1,5 +1,10 @@
 import type { JSXElement, Ref } from '@askrjs/askr/foundations';
 
+export type InputEvent = Event & {
+  currentTarget: HTMLInputElement;
+  target: HTMLInputElement;
+};
+
 export type InputOwnProps = {
   children?: unknown;
   disabled?: boolean;
@@ -19,6 +24,13 @@ export type InputAsChildProps = InputOwnProps & {
   asChild: true;
   children: JSXElement;
   ref?: Ref<unknown>;
+};
+
+export type DebouncedInputProps = Omit<InputInputProps, 'onInput' | 'type'> & {
+  type?: JSX.IntrinsicElements['input']['type'];
+  debounceMs?: number;
+  onInput?: (event: InputEvent) => void;
+  onDebouncedInput?: (value: string) => void;
 };
 
 export type InputProps = InputInputProps | InputAsChildProps;
